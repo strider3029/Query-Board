defe21aea37465abf39bcc0759cc346b53a2a486
B Gray <strider_3029@yahoo.com>
2015-04-28 23:44:29 +1000

    Initial Commit

---
+++ QueryBoard/QueryBoard.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+
+namespace QueryBoard
+{
+    class QueryBoard
+    {
+        static int[,] matrix = new int[256, 256];
+
+        static int Main(string[] args)
+        {
+            // Check the argument and file exists
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                Console.Write("You failed to specify the file to read, or entered a non existant file path.\nPlease try again with the file name as the first argument.");
+                return 0;
+            }
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (null == line) continue;
+
+                    if ("" == line) continue;
+
+                    PerformQuery(line);
+                }
+            }
+
+            return 0;
+        }
+
+        static void PerformQuery(string query)
+        {
+            int sumOfQuery = 0;
+            string[] numberBlocks = query.Split(' ');
+
+            if (numberBlocks.Length != 0)
+            {
+                switch (numberBlocks[0])
+                {
+                    case "SetRow":
+                        {
+                            int row = Convert.ToInt32(numberBlocks[1]);
+                            int value = Convert.ToInt32(numberBlocks[2]);
+
+                            for (int column = 0; column < matrix.GetLength(1); ++column)
+                            {
+                                matrix[row, column] = value;
+                            }
+
+                            break;
+                        }
+                    case "SetCol":
+                        {
+                            int column = Convert.ToInt32(numberBlocks[1]);
+                            int value = Convert.ToInt32(numberBlocks[2]);
+
+                            for (int row = 0; row < matrix.GetLength(0); ++row)
+                            {
+                                matrix[row, column] = value;
+                            }
+
+                            break;
+                        }
+                    case "QueryRow":
+                        {
+                            int row = Convert.ToInt32(numberBlocks[1]);
+
+                            for (int column = 0; column < matrix.GetLength(1); ++column)
+                            {
+                                sumOfQuery += matrix[row, column];
+                            }
+
+                            Console.WriteLine(sumOfQuery);
+                            break;
+                        }
+                    case "QueryCol":
+                        {
+                            int column = Convert.ToInt32(numberBlocks[1]);
+
+                            for (int row = 0; row < matrix.GetLength(0); ++row)
+                            {
+                                sumOfQuery += matrix[row, column];
+                            }
+
+                            Console.WriteLine(sumOfQuery);
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,16 @@
+C# code sample
+
+CodeEval: Query Board
+URL: https://www.codeeval.com/open_challenges/87/
+
+There is a board (matrix). Every cell of the board contains one integer, which is 0 initially. 
+
+The next operations can be applied to the Query Board: 
+SetRow i x: it means that all values in the cells on row "i" have been changed to value "x" after this operation. 
+SetCol j x: it means that all values in the cells on column "j" have been changed to value "x" after this operation. 
+QueryRow i: it means that you should output the sum of values on row "i". 
+QueryCol j: it means that you should output the sum of values on column "j". 
+
+The board's dimensions are 256x256 
+"i" and "j" are integers from 0 to 255 
+"x" is an integer from 0 to 31 
